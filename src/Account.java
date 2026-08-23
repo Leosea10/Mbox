5f8c70363df0fba537e82a0fba7d81b3d3172815
冷冰冰 <3413376942@qq.com>
2026-08-23 10:46:45 +0800

    Add Account class with basic attributes and methods

---
+++ src/Account.java
@@ -0,0 +1,36 @@
+public class Account {
+    private String accountNumber;
+    private String accountType;
+    private double balance;
+
+    public Account(String accountNumber, String accountType, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", accountType='" + accountType + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
